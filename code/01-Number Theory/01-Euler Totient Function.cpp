3d33379d4b04ba58d1d3b46487d6e9c7b35194cf
Faysal Ahammed Chowdhury <faysalahammedchowdhury01@gmail.com>
2025-10-07 15:00:28 +0600

    Added ETF - Number Theory

---
+++ code/01-Number Theory/01-Euler Totient Function.cpp
@@ -0,0 +1,14 @@
+// Time: O($\sqrt{N}$)
+map<int, int> dp; // memo
+int phi(int n) {
+  if (dp.count(n)) return dp[n];
+  int ans = n, m = n;
+  for (int i = 2; i * i <= m; i++) {
+    if (m % i == 0) {
+      while (m % i == 0) m /= i;
+      ans = ans / i * (i - 1);
+    }
+  }
+  if (m > 1) ans = ans / m * (m - 1);
+  return dp[n] = ans;
+}
